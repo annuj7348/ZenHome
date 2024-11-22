19dbd2cc08660c9625e7a7d241b32c6423788cf4
annuj7348 <132692424+annuj7348@users.noreply.github.com>
2024-11-22 20:09:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Simple Script for Navigation Scroll Effect
+document.querySelectorAll('.nav-list a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+  
